daaf54e08a65aac425a0129c958c5e4c19eb974a
Michael Lazar <mlazar@doctorondemand.com>
2020-11-06 23:35:25 -0500

    update settings for a new crawl

--- mozz_archiver/settings/prod.py
+++ mozz_archiver/settings/prod.py
@@ -2,17 +2,15 @@ from mozz_archiver.settings.local import *
 
 WARC_DEBUG = False
 WARC_GZIP = True
-WARC_FILE_PREFIX = 'gemini_oct2020'
+WARC_FILE_PREFIX = 'gemini_nov2020'
 WARC_FILE_DIRECTORY = '/mnt/volume_nyc1_01/'
-WARC_IS_PART_OF = "gemini-crawl-oct2020"
+WARC_IS_PART_OF = "gemini-crawl-nov2020"
 
 LOG_FILE = "crawl.log"
 JOBDIR = f"crawls/{WARC_IS_PART_OF}"
 
 URL_DENY_LIST = [
     "gemini://gemini.spam.works/mirrors/textfiles/",  # Mirror of www.textfiles.com
-    "gemini://gemini.spam.works?]////",  # Infinite redirect
     "gemini://fkfd.me/git/cgi/",  # git frontend, very slow to respond
     "gemini://git.fkfd.me/cgi/",  # git frontend, very slow to respond
-    "gemini://gemini.conman.org/test/redirhell",  # I need to fix my crawler...
 ]
